fdb52043a4ed0aac1716aea23622466fe2fe5f1a
Ian Serlin <ian@ianserlin.com>
2012-04-24 22:49:50 -0700

    twitter integration now works

--- www/TwitterPlugin.js
+++ www/TwitterPlugin.js
@@ -1,25 +1,26 @@
 var Twitter = function(){};
 
 Twitter.prototype.isTwitterAvailable = function(response){
-    cordova.exec(response, null, "TwitterPlugin", "isTwitterAvailable", []);
+    cordova.exec(response, null, "org.apache.cordova.twitter", "isTwitterAvailable", []);
+    //cordova.exec(response, null, "TwitterPlugin", "isTwitterAvailable", []);
 };
 
 Twitter.prototype.isTwitterSetup = function(response){
-    cordova.exec(response, null, "TwitterPlugin", "isTwitterSetup", []);
+    cordova.exec(response, null, "org.apache.cordova.twitter", "isTwitterSetup", []);
 };
 
 Twitter.prototype.composeTweet = function(success, failure, tweetText, options){
     options = options || {};
     options.text = tweetText;
-    cordova.exec(success, failure, "TwitterPlugin", "composeTweet", [options]);
+    cordova.exec(success, failure, "org.apache.cordova.twitter", "composeTweet", [options]);
 };
 
 Twitter.prototype.getPublicTimeline = function(success, failure){
-    cordova.exec(success, failure, "TwitterPlugin", "getPublicTimeline", []);
+    cordova.exec(success, failure, "org.apache.cordova.twitter", "getPublicTimeline", []);
 };
 
 Twitter.prototype.getMentions = function(success, failure){
-    cordova.exec(success, failure, "TwitterPlugin", "getMentions", []);
+    cordova.exec(success, failure, "org.apache.cordova.twitter", "getMentions", []);
 };
 
 cordova.addConstructor(function() {
@@ -33,3 +34,4 @@ cordova.addConstructor(function() {
 					   if(!window.plugins) window.plugins = {};
 					   window.plugins.twitter = new Twitter();
 					   });
+
